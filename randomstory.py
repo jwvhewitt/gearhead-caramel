9c654a07dc3ec05aba4d2d1ad18ecaeb1c14b668
Joseph Hewitt <pyrrho12@yahoo.ca>
2015-09-21 19:49:41 -0700

    Added story test

---
+++ randomstory.py
@@ -0,0 +1,23 @@
+#
+# Test of the story branching system. For each episode, the player can choose
+# one path, which affects the world state and potentially triggers special
+# events.
+#
+# In this case, the actions of the player inevitably lead to the discovery of
+# Typhon, a mission to recover Typhon, and the final battle against Typhon.
+# *Non-standard Game Over: The player becomes Typhon's pilot.
+#
+
+class Plot( object ):
+    pass
+
+class Context( object ):
+    pass
+
+class Character( object ):
+    """A character will have a goal state."""
+
+
+
+
+
